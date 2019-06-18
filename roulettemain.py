3480602663f0dfb5fb69728d71beffe08ae98c31
a4taka <a4taka@gmail.com>
2019-06-18 18:58:24 +0900

    統計機能追加

--- roulettemain.py
+++ roulettemain.py
@@ -1,12 +1,29 @@
 import random
 import os
 import datetime
+import sqlite3
+
+# 試行回数
+N_LOOP = 38
+
+# TSV出力ファイル
+output_filename = "roulette1/data.txt"
+
+# Sqlite3
+# データベースファイルのパス
+dbpath = 'roulette1/rltdb1.db'
+
+# データベース接続とカーソル生成
+connection = sqlite3.connect(dbpath)
+# 自動コミットにする場合は下記を指定（コメントアウトを解除のこと）
+# connection.isolation_level = None
+cursor = connection.cursor()
 
 class Roulette():
 
     def __init__(self,mode):
 
-        self.output_filename = "data.txt"
+        #self.output_filename = "roulette1/data.txt"
 
         self.value = None
         self.color = None
@@ -86,7 +103,7 @@ class Roulette():
         elif value2 == '34': return 'red'
         elif value2 == '35': return 'black'
         elif value2 == '36': return 'red'
-        else:return '-1'
+        else:return 'error'
 
     def CheckEo(self,value2):
         if int(value2) == 0: return 'zero'
@@ -99,24 +116,78 @@ class Roulette():
     
     def OutputList(self):
         print('ファイル書き出し中...' + str(datetime.datetime.now()))
-        file = open(self.output_filename, 'w')
+
+        # 出目のカウント
+        number_count = []
+        zero_count =0
+        red_count = 0
+        black_count = 0
+
+        for i in range(len(self.spotlist)):
+            number_count.append(self.valuehistory.count(self.spotlist[i]))
+
+        #file = open(self.output_filename, 'w')
+        file = open(output_filename, 'w')
+        
         for i in range(len(self.valuehistory)):
-            file.write(self.valuehistory[i]+'\t'+self.colorhistory[i]+'\t'+self.eohistory[i]+'\n')  
+            file.write(self.valuehistory[i]+'\t'+self.colorhistory[i]+'\t'+self.eohistory[i]+'\n') 
+
+        # 改行
+        file.write('\n\n')
+
+        # 出目のカウント
+        for i in range(len(self.spotlist)):
+            file.write(self.spotlist[i]+'\t'+str(number_count[i])+'\n') 
+
+        # 色別のカウント
+        zero_count = self.valuehistory.count('0') + self.valuehistory.count('00')
+        red_count = self.colorhistory.count('red')
+        black_count = self.colorhistory.count('black')
+
+        file.write('zero\t'+str(zero_count)+'\n')
+        file.write('red\t'+str(red_count)+'\n')
+        file.write('black\t'+str(black_count)+'\n')
+
         file.close()
+
         print('ファイル書き出し完了' + str(datetime.datetime.now())) 
+
+    def SaveSqlite(self):
+        print('Sqlite INSERT start:' + str(datetime.datetime.now())) 
+
+        for i in range(len(self.valuehistory)):
+            cursor.execute("INSERT INTO deme VALUES (:id, :value, :color, :eo)",
+                   {'id': i, 'value': self.valuehistory[i], 'color':self.colorhistory[i], 'eo':self.eohistory[i]})
+
+        #cursor.execute("INSERT INTO deme VALUES (:id, :value, :color, :eo)",
+        #           {'id': 1, 'value': '田中', 'color':'blue', 'eo':'test'})
+
+        # 保存を実行（忘れると保存されないので注意）
+        connection.commit()
+
+        # 接続を閉じる
+        connection.close()
+
+        print('Sqlite INSERT finish:' + str(datetime.datetime.now())) 
+
+
 
 # main
 
 rlt = Roulette('a')
 
 print('ルーレット実行中...' + str(datetime.datetime.now()))
-for i in range(100000):
+for i in range(N_LOOP):
     rlt.Do()
 
 print('ルーレット実行完了' + str(datetime.datetime.now()))
 
-rlt.OutputList()
-#rlt.PrintList()
+# データベースに書き出し
+rlt.SaveSqlite()
 
-#print(rlt.GetResult()+'/'+rlt.GetColer()+'/'+rlt.GetEo())
+# ファイルに書き出し
+rlt.OutputList()
+
+
+
 
